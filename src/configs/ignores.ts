728ecc02074bbb277d5991078f908c614e73b7de
Myoschen <myos.chen@gmail.com>
2025-01-22 00:53:00 +0800

    refactor: simplify ignores options handling

--- src/configs/ignores.ts
+++ src/configs/ignores.ts
@@ -1,11 +1,9 @@
 import type { Linter } from 'eslint'
 
-export type IgnoresOptions = {
-  ignores?: string[]
-}
+export type IgnoresOptions = string[]
 
-export function ignores(options: IgnoresOptions = {}): Linter.Config[] {
+export function ignores(options: IgnoresOptions = []): Linter.Config[] {
   return [{
-    ignores: ['**/node_modules/*', ...(options.ignores ?? [])],
+    ignores: ['**/node_modules/*', ...options],
   }]
 }
